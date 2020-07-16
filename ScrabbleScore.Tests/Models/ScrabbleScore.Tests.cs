f5391c75d200161ca691f5a944d5df832b14a74e
Ian Gregg <iangregg188@gmail.com>
2020-07-16 13:22:03 -0700

    Word Constructor passed the red green refactoring.
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- ScrabbleScore.Tests/Models/ScrabbleScore.Tests.cs
+++ ScrabbleScore.Tests/Models/ScrabbleScore.Tests.cs
@@ -1,22 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using Scrabble.Models;
+
 
 namespace ScrabbleScore.Tests
 {
   [TestClass]
-  public class ScrabbleScoreTests : IDisposable
+  public class ScrabbleScoreTests
   {
-    public void Dispose()
-    {
-      ScrabbleScore.ClearAll();
-    }
     [TestMethod]
     public void WordConstructor_WillCreateAnInstanceOfWordObject_Word()
     {
       //Arrange
       Words newWord = new Words("test");
-      Assert.AreEqual(typeof(Item), newWord.GetType());
+      Assert.AreEqual(typeof(Words), newWord.GetType());
     }
     [TestMethod]
     public void RefrenceScore_RefrenceTheScoreForLetters_True()
